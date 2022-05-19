a8111363ae4336b559a6aa37bbbd92d91469f8cc
Jochen Smolka <smolkajochen@gmail.com>
2022-05-19 15:16:12 +0200

    style cleanup

--- elf_main4_display.m
+++ elf_main4_display.m
@@ -1,13 +1,13 @@
-function elf_main4_display(dataset, imgformat)
+function elf_main4_display(dataSet, imgFormat)
 % ELF_MAIN4_DISPLAY simply displays the intensity mean and mean image for a dataset
 %
-% elf_main4_display(dataset, imgformat)
+% elf_main4_display(dataSet, imgFormat)
 
-if nargin < 2 || isempty(imgformat), imgformat = '*.dng'; end
+if nargin < 2 || isempty(imgFormat), imgFormat = '*.dng'; end
 
           elf_paths;
-para    = elf_para('', dataset, imgformat);
-          elf_main3_intsummary(dataset, imgformat)
+para    = elf_para('', dataSet, imgFormat);
+          elf_main3_intsummary(dataSet, imgFormat)
 fh      = elf_support_formatA4(48, 2);
           elf_plot_image(elf_io_readwrite(para, 'loadmeanimg_tif'), [], fh); % open mean image
 
