955a2ecb7578360953b2ec012aea54ed2aab5330
Jochen Smolka <smolkajochen@gmail.com>
2024-02-16 17:25:34 +0100

    Calibrator class to simplify calibration; works but still needs more work

---
+++ classes/Calibrator.m
@@ -0,0 +1,180 @@
+classdef Calibrator
+    %CALIBRATOR Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+        BlackLevel
+        BlackLevelMat
+        WhiteLevel % the manufacturer's white level
+        SaturationLevel % raw counts higher than this (before dark correction) are considered saturated  
+        AbsoluteFactor
+        AbsoluteMat
+        VignettingMat
+        Acf     % Aperture correction factor in new d850 calibration
+    end
+
+    properties(SetAccess=immutable)
+        CameraString
+        Height
+        Width
+    end
+    
+    methods
+        function obj = Calibrator(camString, height, width, blackLevel, whiteLevel, info)
+            %CALIBRATOR Construct an instance of this class
+            %   Detailed explanation goes here
+            obj.CameraString = camString;
+            obj.Height = height;
+            obj.Width = width;
+            obj.BlackLevel = blackLevel;
+            obj.WhiteLevel = whiteLevel;
+            obj = obj.loadCalib(info.DigitalCamera.ExposureTime, info.DigitalCamera.ISOSpeedRatings); % TODO: This info stuff should be removed here somehow, or streamlined
+        end
+        
+        function outputArg = method1(obj,inputArg)
+            %METHOD1 Summary of this method goes here
+            %   Detailed explanation goes here
+            outputArg = obj.Property1 + inputArg;
+        end
+    end
+
+    methods(Hidden, Access=protected)
+        function obj = loadCalib(obj, exp, iso)
+
+            switch lower(obj.CameraString)
+                case {'nikon d800e', 'nikon d810', 'nikon z 6'}
+                    % 1. black and white levels
+                    obj.SaturationLevel = 15520; % 15992 was found in calibration, 15520 is the black value in EXIF file
+                    
+                    % 2. ISO/EXP/APT 2016 calibration
+                    para    = elf_para;
+                    TEMP    = load(fullfile(para.paths.calibfolder, lower('nikon d810'), 'absolute.mat'));
+                    obj.AbsoluteFactor = TEMP.wlcf;
+                    
+                    % 3. Vignetting
+                    obj = obj.getVignMat('nikon d810');
+        
+                case 'nikon d850'
+                    % 1. black and white levels
+                    obj.SaturationLevel = 15520;
+        
+                    % 2. ISO/EXP/APT calibration
+                    para    = elf_para;
+                    TEMP    = load(fullfile(para.paths.calibfolder, obj.CameraString, 'absolute.mat'));
+                    obj.Acf      = TEMP.acf;
+                    obj.AbsoluteFactor = TEMP.wlcf;
+                                
+                    % 3. Vignetting
+                    obj = obj.getVignMat('nikon d810');
+                    
+                otherwise
+                    % For an unknown camera, use no calibration correction; an uncalibrated image is better than none
+                    % 1. black and white levels
+                    obj.SaturationLevel   = 0.95*obj.WhiteLevel;      % white level, this should corresponds to a reasonable saturation level
+        
+                    % 2. ISO/EXP/APT calibration
+                    obj.AbsoluteFactor = [1 1 1];
+                                
+                    % 3. Vignetting
+                    obj = obj.getVignMat('nikon d810');
+        
+                    warning('No intensity calibration available for this camera (%s) ', obj.CameraString);
+            end
+
+            % calculate mats for faster calibration later
+            obj = obj.getBlackLevelMat(exp, iso);
+            obj = obj.getAbsoluteMat();
+
+        end
+
+        function obj = getBlackLevelMat(obj, exp, iso)
+            % Load or calculate black level correction matrix for this camera type, width/height
+            
+            persistent storeBLM;
+            persistent storedBLMName;
+            
+            blmname               = sprintf('%s_%d_%d_%.6f_%d', obj.CameraString, obj.Height, obj.Width, exp, iso);
+            
+            if strcmp(blmname, storedBLMName) && ~isempty(storeBLM)
+                obj.BlackLevelMat = storeBLM;
+            else
+                obj.BlackLevelMat = cat(3, obj.BlackLevel(1) * ones(obj.Height, obj.Width), obj.BlackLevel(2) * ones(obj.Height, obj.Width), obj.BlackLevel(3) * ones(obj.Height, obj.Width));
+                storeBLM          = obj.BlackLevelMat;
+                storedBLMName     = blmname;
+            end
+        end
+        
+        function obj = getAbsoluteMat(obj)
+            % Load or calculate black level correction matrix for this camera type, width/height
+            
+            persistent storeWLCM;
+            persistent storedWLCMName;
+            
+            wlcmname             = sprintf('%s_%d_%d', obj.CameraString, obj.Height, obj.Width);
+            
+            if strcmp(wlcmname, storedWLCMName) && ~isempty(storeWLCM)
+                obj.AbsoluteMat           = storeWLCM;
+            else
+                obj.AbsoluteMat  = cat(3, obj.AbsoluteFactor(1) * ones(obj.Height, obj.Width), ...
+                                          obj.AbsoluteFactor(2) * ones(obj.Height, obj.Width), ...
+                                          obj.AbsoluteFactor(3) * ones(obj.Height, obj.Width));
+                storeWLCM        = obj.AbsoluteMat;
+                storedWLCMName   = wlcmname;
+            end
+        end
+        
+        function obj = getVignMat(obj, camstring)
+            % Load or calculate vignetting correction for this image camera type, width/height
+            % Saving to a file and loading when needed has been tested and takes longer than recalculating on ELFPC (10s v 3s)
+            
+            persistent storedVign;
+            persistent storedVignName;
+            
+            vignname = sprintf('%s_%d_%d', camstring, obj.Height, obj.Width);
+            
+            if strcmp(vignname, storedVignName) && ~isempty(storedVign)
+                obj.VignettingMat    = storedVign;
+            else
+                % Calculate excentricity and vignetting correction, and store in persistents
+                mid     = [1+(obj.Height-1)/2; 1+(obj.Width-1)/2];   % centre of image (x/y, h/w)
+                r_full  = 8 * min([obj.Height obj.Width]) / 24;      % theoretical value for 24mm high chip that is fully covered by fisheye circular image
+                [y, x]  = meshgrid(1:obj.Width, 1:obj.Height);       % x/y positions of all points in the image
+                r_all   = sqrt((x-mid(1)).^2 + (y-mid(2)).^2);
+                exc     = real(asind(r_all / 2 / r_full) * 2);  % remove the complex part that happens for excentricities >90
+        
+                % Calculate vignetting correction
+                para    = elf_para;
+                fname   = fullfile(para.paths.calibfolder, lower(camstring), 'vign_calib.mat');
+                if isfile(fname)
+                    TEMP = load(fname); % holds pf, fitted vignetting-correction function
+                    pf = TEMP.pf;
+                else
+                    warning('No vignetting calibration exists for this camera; not correcting for vignetting');
+                    pf = cat(3, zeros(3, 3), zeros(3, 3), zeros(3, 3), ones(3, 3));
+                end
+                
+                fr      = sub_feval(pf(1, 1, :), exc) / sub_feval(pf(1, 1, :), 0);
+                fg      = sub_feval(pf(1, 2, :), exc) / sub_feval(pf(1, 2, :), 0);
+                fb      = sub_feval(pf(1, 3, :), exc) / sub_feval(pf(1, 3, :), 0);
+                obj.VignettingMat{1} = cat(3, fr, fg, fb);
+                fr      = sub_feval(pf(2, 1, :), exc) / sub_feval(pf(2, 1, :), 0);
+                fg      = sub_feval(pf(2, 2, :), exc) / sub_feval(pf(2, 2, :), 0);
+                fb      = sub_feval(pf(2, 3, :), exc) / sub_feval(pf(2, 3, :), 0);
+                obj.VignettingMat{2} = cat(3, fr, fg, fb);
+                fr      = sub_feval(pf(3, 1, :), exc) / sub_feval(pf(3, 1, :), 0);
+                fg      = sub_feval(pf(3, 2, :), exc) / sub_feval(pf(3, 2, :), 0);
+                fb      = sub_feval(pf(3, 3, :), exc) / sub_feval(pf(3, 3, :), 0);
+                obj.VignettingMat{3} = cat(3, fr, fg, fb);
+                
+                storedVign = obj.VignettingMat;
+                storedVignName = vignname;
+            end
+
+
+            function y = sub_feval(fun, x)
+                y = fun(1)*x.^3 + fun(2)*x.^2 + fun(3)*x + fun(4);
+            end
+        end
+    end
+end
+
--- io/elf_calibrate_abssens.m
+++ io/elf_calibrate_abssens.m
@@ -25,10 +25,10 @@ if ~isa(im, 'double') % If read from elf_io_loaddng, images will usually still b
 end 
 
 %% load calibration factors depending on camera model
-calfactors  = sub_loadCalib(size(im, 1), size(im, 2), info, blackLevel);
+cal = Calibrator(info.Model, size(im, 1), size(im, 2), blackLevel, info.SubIFDs{1}.WhiteLevel(1), info);
 
 %% apply calibration
-[im, conf, confFactors] = sub_applyCalib(im, info, calfactors);
+[im, conf, confFactors] = sub_applyCalib(im, info, cal);
 
 
 % Further calibration (for spectral sensitivity) will be done after HDR calculation in elf_calibrate_spectral
@@ -40,78 +40,21 @@ end % main function
 
 
 %% subfunctions
-function calFactors = sub_loadCalib(height, width, info, blackLevel)
-    
-    % Extract camera parameters and calibration factors
-    exp         = info.DigitalCamera.ExposureTime;      % exposure time in seconds
-    iso         = info.DigitalCamera.ISOSpeedRatings;   % ISO speed
-    camstring   = info.Model;
-
-    calFactors.blackLevel = blackLevel;
-
-    switch lower(camstring)
-        case {'nikon d800e', 'nikon d810', 'nikon z 6'}
-            % 1. black and white levels
-            calFactors.saturation = 15520; % 15992 was found in calibration, 15520 is the black value in EXIF file
-            calFactors.blackLevelMat = sub_getBlackLevelMat(blackLevel, 'nikon d810', height, width, exp, iso);
-            
-            % 2. ISO/EXP/APT 2016 calibration
-            para    = elf_para;
-            TEMP    = load(fullfile(para.paths.calibfolder, lower('nikon d810'), 'absolute.mat'));
-            calFactors.absolute = TEMP.wlcf;
-            calFactors.absMat   = sub_getAbsMat(TEMP.wlcf, camstring, height, width);
-            
-            % 3. Vignetting
-            calFactors.vignMat = sub_getVignMat('nikon d810', height, width);
-
-        case 'nikon d850'
-            % 1. black and white levels
-            calFactors.saturation = 15520;
-            calFactors.blackLevelMat = sub_getBlackLevelMat(blackLevel, camstring, height, width, exp, iso);
-
-            % 2. ISO/EXP/APT calibration
-            para    = elf_para;
-            TEMP    = load(fullfile(para.paths.calibfolder, camstring, 'absolute.mat'));
-            calFactors.acf      = TEMP.acf;
-            calFactors.absolute = TEMP.wlcf;
-            calFactors.absMat   = sub_getAbsMat(TEMP.wlcf, camstring, height, width);
-                        
-            % 3. Vignetting
-            calFactors.vignMat = sub_getVignMat('nikon d810', height, width);
-            
-        otherwise
-            % For an unknown camera, use no calibration correction; an uncalibrated image is better than none
-            % 1. black and white levels
-            calFactors.saturation   = 0.95*info(1).SubIFDs{1}.WhiteLevel(1);      % white level, this should corresponds to a reasonable saturation level
-            calFactors.blackLevelMat = sub_getBlackLevelMat(blackLevel, camstring, height, width, exp, iso);
-
-            % 2. ISO/EXP/APT calibration
-            calFactors.absolute = [1 1 1];
-            calFactors.absMat   = sub_getAbsMat([1 1 1], camstring, height, width);
-                        
-            % 3. Vignetting
-            calFactors.vignMat = sub_getVignMat('nikon d810', height, width);
-
-            warning('No intensity calibration available for this camera (%s) ', camstring);
-    end
-end
-
-function [im, conf, confFactors] = sub_applyCalib(im, info, calFactors)
+function [im, conf, confFactors] = sub_applyCalib(im, info, cal)
 
     % Extract camera parameters and calibration factors
     exp         = info.DigitalCamera.ExposureTime;      % exposure time in seconds
     iso         = info.DigitalCamera.ISOSpeedRatings;   % ISO speed
     apt         = info.DigitalCamera.FNumber;           % Aperture F-Stop
-    camstring   = info.Model;
 
     %% Apply calibration
     % Subtract the camera's black level (saturation level has to be taken into account later)
-    im          = im - calFactors.blackLevelMat;
+    im          = im - cal.BlackLevelMat;
     conf        = im; % Use these raw values (after dark subtraction) to define every pixel's confidence: 
                       % In HDR calculation, each HDR pixel will be assigned the radiance value it holds in the image where it has the highest confidence 
-    confFactors = calFactors.saturation - calFactors.blackLevel(:)';
+    confFactors = cal.SaturationLevel - cal.BlackLevel(:)';
 
-    switch lower(camstring)
+    switch lower(cal.CameraString)
         case {'nikon d800e', 'nikon d810', 'nikon z 6'}
             % 2. ISO/EXP/APT 2016 calibration
 
@@ -125,7 +68,7 @@ function [im, conf, confFactors] = sub_applyCalib(im, info, calFactors)
         case 'nikon d850'
 
             % 2. ISO/EXP/APT calibration
-            acf     = calFactors.acf(calFactors.acf(:, 1)==apt, 2);
+            acf     = cal.Acf(cal.Acf(:, 1)==apt, 2);
             settingFactor  = exp * iso * acf;
         
         otherwise
@@ -133,7 +76,7 @@ function [im, conf, confFactors] = sub_applyCalib(im, info, calFactors)
     end   
     
     % correct for exposure time, ISO setting (gain) and aperture
-    im          = im ./ settingFactor ./ calFactors.absMat;            % counts per second per ISO per aperture
+    im          = im ./ settingFactor ./ cal.AbsoluteMat;            % counts per second per ISO per aperture
     
     % correct for vignetting
     switch apt
@@ -146,93 +89,8 @@ function [im, conf, confFactors] = sub_applyCalib(im, info, calFactors)
         otherwise
             error('Aperture %g currently not supported.', apt);
     end
-    im          = im ./ calFactors.vignMat{apInd};
+    im          = im ./ cal.VignettingMat{apInd};
 
 end
 
-function blm = sub_getBlackLevelMat(blackLevel, camstring, height, width, exp, iso)
-    % Load or calculate black level correction matrix for this camera type, width/height
-    
-    persistent storeBLM;
-    persistent storedBLMName;
-    
-    blmname             = sprintf('%s_%d_%d_%.6f_%d', camstring, height, width, exp, iso);
-    
-    if strcmp(blmname, storedBLMName) && ~isempty(storeBLM)
-        blm             = storeBLM;
-    else
-        blm             = cat(3, blackLevel(1) * ones(height, width), blackLevel(2) * ones(height, width), blackLevel(3) * ones(height, width));
-        storeBLM        = blm;
-        storedBLMName   = blmname;
-    end
-end
-
-function absMat = sub_getAbsMat(wlcf, camstring, height, width)
-    % Load or calculate black level correction matrix for this camera type, width/height
-    
-    persistent storeWLCM;
-    persistent storedWLCMName;
-    
-    wlcmname              = sprintf('%s_%d_%d', camstring, height, width);
-    
-    if strcmp(wlcmname, storedWLCMName) && ~isempty(storeWLCM)
-        absMat           = storeWLCM;
-    else
-        absMat           = cat(3, wlcf(1) * ones(height, width), wlcf(2) * ones(height, width), wlcf(3) * ones(height, width));
-        storeWLCM        = absMat;
-        storedWLCMName   = wlcmname;
-    end
-end
-
-function vign = sub_getVignMat(camstring, height, width)
-    % Load or calculate vignetting correction for this image camera type, width/height
-    % Saving to a file and loading when needed has been tested and takes longer than recalculating on ELFPC (10s v 3s)
-    
-    persistent storedVign;
-    persistent storedVignName;
-    
-    vignname = sprintf('%s_%d_%d', camstring, height, width);
-    
-    if strcmp(vignname, storedVignName) && ~isempty(storedVign)
-        vign    = storedVign;
-    else
-        % Calculate excentricity and vignetting correction, and store in persistents
-        mid     = [1+(height-1)/2; 1+(width-1)/2];   % centre of image (x/y, h/w)
-        r_full  = 8 * min([height width]) / 24;      % theoretical value for 24mm high chip that is fully covered by fisheye circular image
-        [y, x]  = meshgrid(1:width, 1:height);       % x/y positions of all points in the image
-        r_all   = sqrt((x-mid(1)).^2 + (y-mid(2)).^2);
-        exc     = real(asind(r_all / 2 / r_full) * 2);  % remove the complex part that happens for excentricities >90
-
-        % Calculate vignetting correction
-        para    = elf_para;
-        fname   = fullfile(para.paths.calibfolder, lower(camstring), 'vign_calib.mat');
-        if isfile(fname)
-            TEMP    = load(fname); % holds pf, fitted vignetting-correction function
-            pf = TEMP.pf;
-        else
-            warning('No vignetting calibration exists for this camera; not correcting for vignetting');
-            pf = cat(3, zeros(3, 3), zeros(3, 3), zeros(3, 3), ones(3, 3));
-        end
-        
-        fr      = sub_feval(pf(1, 1, :), exc) / sub_feval(pf(1, 1, :), 0);
-        fg      = sub_feval(pf(1, 2, :), exc) / sub_feval(pf(1, 2, :), 0);
-        fb      = sub_feval(pf(1, 3, :), exc) / sub_feval(pf(1, 3, :), 0);
-        vign{1} = cat(3, fr, fg, fb);
-        fr      = sub_feval(pf(2, 1, :), exc) / sub_feval(pf(2, 1, :), 0);
-        fg      = sub_feval(pf(2, 2, :), exc) / sub_feval(pf(2, 2, :), 0);
-        fb      = sub_feval(pf(2, 3, :), exc) / sub_feval(pf(2, 3, :), 0);
-        vign{2} = cat(3, fr, fg, fb);
-        fr      = sub_feval(pf(3, 1, :), exc) / sub_feval(pf(3, 1, :), 0);
-        fg      = sub_feval(pf(3, 2, :), exc) / sub_feval(pf(3, 2, :), 0);
-        fb      = sub_feval(pf(3, 3, :), exc) / sub_feval(pf(3, 3, :), 0);
-        vign{3} = cat(3, fr, fg, fb);
-        
-        storedVign = vign;
-        storedVignName = vignname;
-    end
-end
-
-function y = sub_feval(fun, x)
-    y = fun(1)*x.^3 + fun(2)*x.^2 + fun(3)*x + fun(4);
-end
 
