836105959c4a55a742cb2c94981a02c642f8b531
Jochen Smolka <smolkajochen@gmail.com>
2024-08-29 15:55:21 +0200

    All Calibrator persistent variables moved to Static functions, to carry over between data sets

--- classes/Calibrator.m
+++ classes/Calibrator.m
@@ -83,15 +83,19 @@ classdef Calibrator
             %CALIBRATOR Construct an instance of this class
             %   Detailed explanation goes here
             if nargin<3, spectralMethod = "col"; end
+            Logger.log(LogLevel.INFO, 'Creating a Calibrator object for %s camera\n', camString)
             obj.CameraString = camString;
             obj.Width  = wh(1);
             obj.Height = wh(2);
             obj.SpectralMethod = spectralMethod;
-            obj = obj.loadAbsoluteCalib(); % load the absolute factors and vignetting correction
-            obj = obj.loadSpectralCalibration();
+            obj = obj.loadAbsoluteCalib(); % load the absolute and vignetting correction
+            obj = obj.loadSpectralCalibration(); % load the spectral correction
         end
     end
 
+    %%%%%%%%%%%%%%%%%%%%
+    %% PUBLIC METHODS %%
+    %%%%%%%%%%%%%%%%%%%%
     methods
         function [im, conf, confFactors] = applyAbsolute(obj, im, info)
 
@@ -102,7 +106,7 @@ classdef Calibrator
         
             %% Apply calibration
             % Subtract the camera's black level (saturation level has to be taken into account later)
-            im          = im - obj.getBlackLevelMat(info.blackLevels);
+            im          = im - Calibrator.getBlackLevelMat(obj.Height, obj.Width, info.blackLevels);
             conf        = im; % Use these raw values (after dark subtraction) to define every pixel's confidence: 
                               % In HDR calculation, each HDR pixel will be assigned the radiance value it holds in the image where it has the highest confidence 
             confFactors = obj.getSaturationLevel(info) - info.blackLevels(:)';
@@ -192,8 +196,14 @@ classdef Calibrator
     end
 
 
+    %%%%%%%%%%%%%%%%%%%%%%%%
+    %% ABSOLUTE FUNCTIONS %%
+    %%%%%%%%%%%%%%%%%%%%%%%%
     methods(Hidden, Access=protected)
         function obj = loadAbsoluteCalib(obj)
+            % Calibrator.loadAbsoluteCalib loads the absolute and vignetting calibration matrix for this Calibrator object from file
+            % The matrices are stored in obj.VignettingMat and obj.AbsoluteMat, from where it is later used in obj.applyAbsolute
+            Logger.log(LogLevel.INFO, '\tCreating/loading vignetting correction matrix\n')
             switch lower(obj.CameraString)
                 case {'nikon d800e', 'nikon d810', 'nikon z 6'}
                     
@@ -203,7 +213,7 @@ classdef Calibrator
                     obj.AbsoluteFactor = TEMP.wlcf;
                     
                     % 2. Vignetting
-                    obj = obj.getVignMat('nikon d810');
+                    obj.VignettingMat = Calibrator.getVignMat('nikon d810', obj.Height, obj.Width);
         
                 case 'nikon d850'
         
@@ -214,7 +224,7 @@ classdef Calibrator
                     obj.AbsoluteFactor = TEMP.wlcf;
                                 
                     % 2. Vignetting
-                    obj = obj.getVignMat('nikon d810');
+                    obj.VignettingMat = Calibrator.getVignMat('nikon d810', obj.Height, obj.Width);
                     
                 otherwise
                     % For an unknown camera, use no calibration correction; an uncalibrated image is better than none
@@ -223,18 +233,20 @@ classdef Calibrator
                     obj.AbsoluteFactor = [1 1 1];
                                 
                     % 2. Vignetting
-                    obj = obj.getVignMat('nikon d810');
+                    obj.VignettingMat = Calibrator.getVignMat('nikon d810', obj.Height, obj.Width);
         
                     warning('No intensity calibration available for this camera (%s) ', obj.CameraString);
             end
 
             % calculate mats for faster calibration later
-            obj = obj.getAbsoluteMat();
+            Logger.log(LogLevel.INFO, '\tCreating/loading absolute sensitivity correction matrix\n')
+            obj.AbsoluteMat = Calibrator.getAbsoluteMat(obj.CameraString, obj.Height, obj.Width, obj.AbsoluteFactor);
         end
 
 
         function satLevel = getSaturationLevel(obj, info)
-            % returns the saturation level for this camera (before dark correction
+            % Calibrator.getSaturationLevel returns the saturation level for this camera (before dark correction)
+
             switch lower(obj.CameraString)
                 case {'nikon d800e', 'nikon d810', 'nikon z 6', 'nikon d850'}
                     satLevel = 15520; % 15992 was found in d810 calibration, 15520 is the black value in EXIF file
@@ -242,65 +254,82 @@ classdef Calibrator
                     satLevel = 0.95*info.SubIFDs{1}.WhiteLevel(1);      % white level, this should corresponds to a reasonable saturation level
             end
         end
+    end
 
-
-        function blackLevelMat = getBlackLevelMat(obj, blackLevels)
-            % Load or calculate black level correction matrix for this camera type, width/height
+    methods (Static)
+        function blackLevelMat = getBlackLevelMat(height, width, blackLevels)            
+            % Calibrator.getAbsoluteMat loads or calculates the black level correction matrix for this image width and image height.
+            % This is called during obj.applyAbsolute with the blackLevels for one particular frame.
+            % The matrix is saved in a static class variable, so that at the next frame/dataset, it does not
+            % need to be re-loaded from file, as long as image width and height stay the same.
             
             persistent storeBLM;
             persistent storedBLMName;
             
-            blmname               = sprintf('%d_%d', obj.Height, obj.Width);
+            blmname               = sprintf('%d_%d', height, width);
             
             if strcmp(blmname, storedBLMName) && ~isempty(storeBLM)
                 blackLevelMat = storeBLM;
             else
-                blackLevelMat = ones(obj.Height, obj.Width, 3);
-                storeBLM          = blackLevelMat;
-                storedBLMName     = blmname;
+                blackLevelMat = ones(height, width, 3);
+                storeBLM      = blackLevelMat;
+                storedBLMName = blmname;
             end
 
             blackLevelMat(:, :, 1) = blackLevels(1);
             blackLevelMat(:, :, 2) = blackLevels(2);
             blackLevelMat(:, :, 3) = blackLevels(3);
         end
-        
 
-        function obj = getAbsoluteMat(obj)
-            % Load or calculate black level correction matrix for this camera type, width/height
+
+        function absMat = getAbsoluteMat(camstring, height, width, absoluteFactor)
+            % Calibrator.getAbsoluteMat loads or calculates the absolute sensitivity correction matrix for this camera type, image width and image height.
+            % This is called during obj.loadAbsoluteCalibration.
+            % The matrix is saved in a static class variable, so that the next time a Calibrator object for the same camera is created, it does not
+            % need to be re-loaded from file.
             
             persistent storeWLCM;
             persistent storedWLCMName;
             
-            wlcmname             = sprintf('%s_%d_%d', obj.CameraString, obj.Height, obj.Width);
+            wlcmname             = sprintf('%s_%d_%d', camstring, height, width);
             
             if strcmp(wlcmname, storedWLCMName) && ~isempty(storeWLCM)
-                obj.AbsoluteMat           = storeWLCM;
+                Logger.log(LogLevel.DEBUG, '\t\tUsing stored matrix\n')
+                absMat           = storeWLCM;
             else
-                obj.AbsoluteMat  = cat(3, obj.AbsoluteFactor(1) * ones(obj.Height, obj.Width), ...
-                                          obj.AbsoluteFactor(2) * ones(obj.Height, obj.Width), ...
-                                          obj.AbsoluteFactor(3) * ones(obj.Height, obj.Width));
-                storeWLCM        = obj.AbsoluteMat;
+                Logger.log(LogLevel.DEBUG, '\t\tCalculating new matrix\n')
+                absMat = ones(height, width, 3);
+                absMat(:, :, 1) = absoluteFactor(1);
+                absMat(:, :, 2) = absoluteFactor(2);
+                absMat(:, :, 3) = absoluteFactor(3);
+                storeWLCM        = absMat;
                 storedWLCMName   = wlcmname;
             end
         end
-        
-        function obj = getVignMat(obj, camstring)
-            % Load or calculate vignetting correction for this image camera type, width/height
+
+
+        function vignMat = getVignMat(camstring, height, width)
+            % Calibrator.getVignMat loads or calculates the vignetting matrix for this camera type, image width and image height.
+            % This is called during obj.loadAbsoluteCalibration.
+            % The matrix is saved in a static class variable, so that the next time a Calibrator object for the same camera is created, it does not
+            % need to be re-loaded from file.
+            %
             % Saving to a file and loading when needed has been tested and takes longer than recalculating on ELFPC (10s v 3s)
             
             persistent storedVign;
             persistent storedVignName;
             
-            vignname = sprintf('%s_%d_%d', camstring, obj.Height, obj.Width);
+            vignname = sprintf('%s_%d_%d', camstring, height, width);
             
             if strcmp(vignname, storedVignName) && ~isempty(storedVign)
-                obj.VignettingMat    = storedVign;
+                Logger.log(LogLevel.DEBUG, '\t\tUsing stored matrix\n')
+                vignMat    = storedVign;
             else
+                Logger.log(LogLevel.DEBUG, '\t\tCalculating new matrix\n')
                 % Calculate excentricity and vignetting correction, and store in persistents
-                mid     = [1+(obj.Height-1)/2; 1+(obj.Width-1)/2];   % centre of image (x/y, h/w)
-                r_full  = 8 * min([obj.Height obj.Width]) / 24;      % theoretical value for 24mm high chip that is fully covered by fisheye circular image
-                [y, x]  = meshgrid(1:obj.Width, 1:obj.Height);       % x/y positions of all points in the image
+                mid     = [1+(height-1)/2; 1+(width-1)/2];   % centre of image (x/y, h/w)
+                r_full  = 8 * min([height width]) / 24;      % theoretical value for 24mm high chip that is fully covered by fisheye circular image
+                [y, x]  = meshgrid(1:width, 1:height);       % x/y positions of all points in the image
                 r_all   = sqrt((x-mid(1)).^2 + (y-mid(2)).^2);
                 exc     = real(asind(r_all / 2 / r_full) * 2);  % remove the complex part that happens for excentricities >90
         
@@ -318,17 +347,17 @@ classdef Calibrator
                 fr      = sub_feval(pf(1, 1, :), exc) / sub_feval(pf(1, 1, :), 0);
                 fg      = sub_feval(pf(1, 2, :), exc) / sub_feval(pf(1, 2, :), 0);
                 fb      = sub_feval(pf(1, 3, :), exc) / sub_feval(pf(1, 3, :), 0);
-                obj.VignettingMat{1} = cat(3, fr, fg, fb);
+                vignMat{1} = cat(3, fr, fg, fb);
                 fr      = sub_feval(pf(2, 1, :), exc) / sub_feval(pf(2, 1, :), 0);
                 fg      = sub_feval(pf(2, 2, :), exc) / sub_feval(pf(2, 2, :), 0);
                 fb      = sub_feval(pf(2, 3, :), exc) / sub_feval(pf(2, 3, :), 0);
-                obj.VignettingMat{2} = cat(3, fr, fg, fb);
+                vignMat{2} = cat(3, fr, fg, fb);
                 fr      = sub_feval(pf(3, 1, :), exc) / sub_feval(pf(3, 1, :), 0);
                 fg      = sub_feval(pf(3, 2, :), exc) / sub_feval(pf(3, 2, :), 0);
                 fb      = sub_feval(pf(3, 3, :), exc) / sub_feval(pf(3, 3, :), 0);
-                obj.VignettingMat{3} = cat(3, fr, fg, fb);
+                vignMat{3} = cat(3, fr, fg, fb);
                 
-                storedVign = obj.VignettingMat;
+                storedVign = vignMat;
                 storedVignName = vignname;
             end
 
@@ -337,17 +366,28 @@ classdef Calibrator
                 y = fun(1)*x.^3 + fun(2)*x.^2 + fun(3)*x + fun(4);
             end
         end
+    end
 
-        %% SPECTRAL FUNCTIONS
+
+    %%%%%%%%%%%%%%%%%%%%%%%%
+    %% SPECTRAL FUNCTIONS %%
+    %%%%%%%%%%%%%%%%%%%%%%%%
+    methods(Hidden, Access=protected)
         function obj = loadSpectralCalibration(obj)
+            % Calibrator.loadSpectralCalibration loads the spectral calibration matrix for this Calibrator object from file
+            % The correct matrix (depending on obj.SpectralMethod) is extracted and stored in obj.SpectralMatrix, from where it is later used in obj.applySpectral
 
+            Logger.log(LogLevel.INFO, '\tCreating/loading spectral correction matrix\n')
+
+            % determine which file to load
             switch lower(obj.CameraString)
                 case {'nikon d800e', 'nikon d810', 'nikon z 6'}
                     camstring = 'nikon d810'; % use d810 calibration for all these models
                 otherwise
-                    camstring = obj.CameraString;
+                    camstring = lower(obj.CameraString);
             end
 
+            % load from file, and extract the right matrix depending on obj.SpectralMethod
             switch obj.SpectralMethod
                 case 'colmat' % Full deconvolution of channels to reconstruct a spectrum that is flat between 400-500, 500-600 and 600-700 nm
                     obj.SpectralMatrix  = obj.getColMat(camstring);                                
@@ -356,22 +396,28 @@ classdef Calibrator
                 case 'wb'
                     % Uses the white balance multipliers from each files exif information
             end
-
         end
-     
+    end
 
-        function colmat = getColMat(obj, camstring)
-            % load or calculate color correction matrix for this image camera type
+    methods (Static)
+        function colmat = getColMat(camstring)
+            % Calibrator.getColMat loads or calculates the color correction matrix for this camera type.
+            % This is called during obj.loadSpectralCalibration if obj.SpectralMethod is "colmat".
+            % The matrix is saved in a static class variable, so that the next time a Calibrator object for the same camera is created, it does not
+            % need to be re-loaded from file.
+            %
             % Saving to a file and loading when needed has been tested and takes longer than recalculating on ELFPC (0.8s v 0.2s)
             
             persistent storedColMat;
             persistent storedColMatName;
                 
             if strcmp(camstring, storedColMatName) && ~isempty(storedColMat)
-                colmat  = storedColMat;
+                Logger.log(LogLevel.DEBUG, '\t\tUsing stored matrix\n')
+                colmat = storedColMat;
             else % load from file
-                para    = elf_para;
-                fname = fullfile(para.paths.calibfolder, lower(camstring), 'rgb_calib.mat');
+                Logger.log(LogLevel.DEBUG, '\t\tCalculating new matrix\n')
+                para   = elf_para;
+                fname  = fullfile(para.paths.calibfolder, lower(camstring), 'rgb_calib.mat');
                 if isfile(fname)
                     TEMP    = load(fname, 'colmat');  
                     colmat  = TEMP.colmat;
@@ -385,17 +431,23 @@ classdef Calibrator
         end
         
 
-        function col = getCol(obj, camstring)
-            % load or calculate color correction vector for this image camera type
+        function col = getCol(camstring)
+            % Calibrator.getCol loads or calculates the color correction vector for this camera type.
+            % This is called during obj.loadSpectralCalibration if obj.SpectralMethod is "col".
+            % The matrix is saved in a static class variable, so that the next time a Calibrator object for the same camera is created, it does not
+            % need to be re-loaded from file.
+            %
             % Saving to a file and loading when needed has been tested and takes longer than recalculating on ELFPC (0.8s v 0.2s)
             
             persistent storedCol;
             persistent storedColName;
                 
             if strcmp(camstring, storedColName) && ~isempty(storedCol)
-                col     = storedCol;
+                Logger.log(LogLevel.DEBUG, '\t\tUsing stored matrix\n')
+                col   = storedCol;
             else % load from file
-                para    = elf_para;
+                Logger.log(LogLevel.DEBUG, '\t\tCalculating new matrix\n')
+                para  = elf_para;
                 fname = fullfile(para.paths.calibfolder, lower(camstring), 'rgb_calib.mat');
                 if isfile(fname)
                     TEMP    = load(fname, 'col');            
